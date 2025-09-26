b2b120e5784b1cfa7805c6dd51571626ccbf2942
Aidan Cunniffe <acunniffe@gmail.com>
2025-09-26 20:05:35 +0200

    add factor function

--- abc.ts
+++ abc.ts
@@ -9,3 +9,26 @@ function reverse_number_line(numbers: number[]) {
 function flip_even_odd(numbers: number[]): number[] {
   return numbers.map((num) => (num % 2 === 0 ? num + 1 : num - 1));
 }
+
+function factor_number(num: number): number[] {
+  if (num < 1 || num >= 1000) {
+    throw new Error("Number must be between 1 and 999");
+  }
+
+  const factors: number[] = [];
+  let divisor = 2;
+
+  while (num > 1 && divisor <= Math.sqrt(num)) {
+    while (num % divisor === 0) {
+      factors.push(divisor);
+      num /= divisor;
+    }
+    divisor++;
+  }
+
+  if (num > 1) {
+    factors.push(num);
+  }
+
+  return factors;
+}
