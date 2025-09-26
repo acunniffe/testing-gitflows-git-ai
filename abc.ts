78788430844d8ccc064e7da1327c374402efc232
Aidan Cunniffe <acunniffe@gmail.com>
2025-09-26 14:07:29 -0400

    snake casify (#3)
    * snake casify
    
    * add factor function

--- abc.ts
+++ abc.ts
@@ -1,11 +1,34 @@
-function reverseAlphabet(): string {
+function reverse_alphabet(): string {
   return "zyxwvutsrqponmlkjihgfedcba";
 }
 
-function reverseNumberLine(numbers: number[]) {
+function reverse_number_line(numbers: number[]) {
   return numbers.reverse();
 }
 
-function flipEvenOdd(numbers: number[]): number[] {
+function flip_even_odd(numbers: number[]): number[] {
   return numbers.map((num) => (num % 2 === 0 ? num + 1 : num - 1));
+}
+
+function factor_number(num: number): number[] {
+  if (num < 1 || num >= 1000) {
+    throw new Error("Number must be between 1 and 999");
+  }
+
+  const factors: number[] = [];
+  let divisor = 2;
+
+  while (num > 1 && divisor <= Math.sqrt(num)) {
+    while (num % divisor === 0) {
+      factors.push(divisor);
+      num /= divisor;
+    }
+    divisor++;
+  }
+
+  if (num > 1) {
+    factors.push(num);
+  }
+
+  return factors;
 }
