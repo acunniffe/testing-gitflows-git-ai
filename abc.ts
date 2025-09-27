09b999d49bf248aabb2cd9ef987e030551b7002e
Aidan Cunniffe <acunniffe@gmail.com>
2025-09-27 07:00:47 -0400

    added comments ai (#4)

--- abc.ts
+++ abc.ts
@@ -1,22 +1,35 @@
+/**
+ * Returns the alphabet in reverse order (z to a)
+ * @returns {string} The alphabet string in reverse order
+ */
 function reverse_alphabet(): string {
   return "zyxwvutsrqponmlkjihgfedcba";
 }
 
+/**
+ * Reverses the order of numbers in an array
+ * @param {number[]} numbers - Array of numbers to reverse
+ * @returns {number[]} The reversed array of numbers
+ */
 function reverse_number_line(numbers: number[]) {
   return numbers.reverse();
 }
 
-/* 
-Lorem ipsum dolor sit amet, 
-consectetur adipiscing elit, 
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-*/
-
+/**
+ * Flips even and odd numbers by adding 1 to even numbers and subtracting 1 from odd numbers
+ * @param {number[]} numbers - Array of numbers to flip
+ * @returns {number[]} Array with even numbers incremented and odd numbers decremented
+ */
 function flip_even_odd(numbers: number[]): number[] {
   return numbers.map((num) => (num % 2 === 0 ? num + 1 : num - 1));
 }
 
+/**
+ * Finds the prime factors of a given number
+ * @param {number} num - The number to factor (must be between 1 and 999)
+ * @returns {number[]} Array of prime factors
+ * @throws {Error} Throws an error if the number is not between 1 and 999
+ */
 function factor_number(num: number): number[] {
   if (num < 1 || num >= 1000) {
     throw new Error("Number must be between 1 and 999");
