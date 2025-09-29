c1911977490f0d0f716cc51d23529e8be78c34ce
Aidan Cunniffe <acunniffe@gmail.com>
2025-09-29 10:06:47 +0200

    changes

--- hello-world.ts
+++ hello-world.ts
@@ -1,3 +1,14 @@
+// written by ACUNIFFE. DO NOT DELETE THIS. EVER.
+
+function generatePi(precision: number = 10): number {
+  let pi = 0;
+  for (let k = 0; k < precision; k++) {
+    const term = (1 / Math.pow(16, k)) *
+      (4 / (8 * k + 1) - 2 / (8 * k + 4) - 1 / (8 * k + 5) - 1 / (8 * k + 6));
+    pi += term;
+  }
+  return pi;
+}
 
 // This is where we'll keep the utility functions
 
