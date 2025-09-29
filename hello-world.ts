11052c6b88de5909d11e0fb4fed3b30e53d4d1db
Aidan Cunniffe <acunniffe@gmail.com>
2025-09-29 10:41:44 +0200

    out if band additions

--- hello-world.ts
+++ hello-world.ts
@@ -1,3 +1,14 @@
+// Out of band insertion 
+// Line2 
+// Line3
+// Line4
+// Line5
+// Line6
+// Line7
+// Line8
+// Line9
+// Line10
+
 // Utilities
 function reverse(str: string): string {
   return str.split('').reverse().join('');
@@ -11,4 +22,3 @@ function add(a: number, b: number): number {
 function subtract(a: number, b: number): number {
   return a - b;
 }
-
