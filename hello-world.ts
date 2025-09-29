22ab8a64bee45d9292f680f07a8f79234c2cb9d5
Aidan Cunniffe <acunniffe@gmail.com>
2025-09-29 04:22:34 -0400

    2 commits, on a feature branch (#5)
    * add two numbers
    
    * subtract

--- hello-world.ts
+++ hello-world.ts
@@ -2,3 +2,13 @@
 function reverse(str: string): string {
   return str.split('').reverse().join('');
 }
+// Add two numbers
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+// Subtract two numbers
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
