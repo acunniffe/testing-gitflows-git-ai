f2720f79224277a80077506447134d4cc0e35b31
Aidan Cunniffe <acunniffe@gmail.com>
2025-09-29 10:38:34 +0200

    count primes

--- hello-world.ts
+++ hello-world.ts
@@ -21,6 +21,20 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
+function countPrimes(numbers: number[]): number {
+  function isPrime(n: number): boolean {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 === 0 || n % 3 === 0) return false;
+    for (let i = 5; i * i <= n; i += 6) {
+      if (n % i === 0 || n % (i + 2) === 0) return false;
+    }
+    return true;
+  }
+
+  return numbers.filter(isPrime).length;
+}
+
 // Subtract two numbers
 function subtract(a: number, b: number): number {
   return a - b;
