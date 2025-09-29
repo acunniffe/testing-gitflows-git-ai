f7c503910300df06298982b05749d7391d409361
Aidan Cunniffe <acunniffe@gmail.com>
2025-09-29 10:37:08 +0200

    add count vowels with ai

--- hello-world.ts
+++ hello-world.ts
@@ -1,9 +1,14 @@
 
+function countVowels(str: string): number {
+  const vowels = 'aeiouAEIOU';
+  return str.split('').filter(char => vowels.includes(char)).length;
+}
+
 /*
-"Lorem ipsum dolor sit amet, 
-consectetur adipiscing elit, 
-sed do eiusmod tempor incididunt 
-ut labore et dolore magna aliqua. 
+"Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 */
 
@@ -20,4 +25,5 @@ function add(a: number, b: number): number {
 function subtract(a: number, b: number): number {
   return a - b;
 }
+
 
