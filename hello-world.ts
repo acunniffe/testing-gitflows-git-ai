b64cf05418b0c4796d3dd4678fe42853834c4448
Aidan Cunniffe <acunniffe@gmail.com>
2025-09-29 09:41:01 +0200

    clear it out

--- hello-world.ts
+++ hello-world.ts
@@ -1,14 +1,4 @@
 
-function generatePi(precision: number = 10): number {
-  let pi = 0;
-  for (let k = 0; k < precision; k++) {
-    const term = (1 / Math.pow(16, k)) *
-      (4 / (8 * k + 1) - 2 / (8 * k + 4) - 1 / (8 * k + 5) - 1 / (8 * k + 6));
-    pi += term;
-  }
-  return pi;
-}
-
 // This is where we'll keep the utility functions
 
 function add(a: number, b: number): number {
