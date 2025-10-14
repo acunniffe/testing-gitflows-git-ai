a89a1c702f2004ce0e7b03669e047f11b46b42e2
Aidan Cunniffe <acunniffe@gmail.com>
2025-10-14 15:03:17 -0400

    Branch123 (#8)
    * made changes
    
    * abc

---
+++ harry-potter-books.ts
@@ -0,0 +1,8 @@
+
+export const BOOK_1 = "Harry Potter and the Sorcerer's Stone";
+export const BOOK_2 = "Harry Potter and the Chamber of Secrets";
+export const BOOK_3 = "Harry Potter and the Prisoner of Azkaban";
+export const BOOK_4 = "Harry Potter and the Goblet of Fire";
+export const BOOK_5 = "Harry Potter and the Order of the Phoenix";
+export const BOOK_6 = "Harry Potter and the Half-Blood Prince";
+export const BOOK_7 = "Harry Potter and the Deathly Hallows";
