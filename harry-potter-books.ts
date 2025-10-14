973590a50a91081b91b11e5615a3437f137f2985
Aidan Cunniffe <acunniffe@gmail.com>
2025-10-14 15:11:23 -0400

    feat/abc (#10)
    * made changes
    
    * abc
    
    * example

--- harry-potter-books.ts
+++ harry-potter-books.ts
@@ -1,8 +1,16 @@
 
 export const BOOK_1 = "Harry Potter and the Sorcerer's Stone";
+// Aidan's favorite 
+export const BOOK_2 = "Harry Potter and the Chamber of Secrets";
+//
 export const BOOK_2 = "Harry Potter and the Chamber of Secrets";
 export const BOOK_3 = "Harry Potter and the Prisoner of Azkaban";
 export const BOOK_4 = "Harry Potter and the Goblet of Fire";
 export const BOOK_5 = "Harry Potter and the Order of the Phoenix";
 export const BOOK_6 = "Harry Potter and the Half-Blood Prince";
 export const BOOK_7 = "Harry Potter and the Deathly Hallows";
+
+export const BOOK_8 = "The Fellowship of the Ring";
+export const BOOK_9 = "The Two Towers";
+export const BOOK_10 = "The Return of the King";
+
