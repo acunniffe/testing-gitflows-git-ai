5d4800404905d27d05f542b8100de69d488d377c
Aidan Cunniffe <acunniffe@gmail.com>
2025-10-19 16:35:06 -0400

    ai code

--- other-stuff/file-abc.ts
+++ other-stuff/file-abc.ts
@@ -1,4 +1,14 @@
-
+function fibonacci(n: number): number {
+  if (n <= 1) return n;
+  
+  let a = 0, b = 1;
+  for (let i = 2; i <= n; i++) {
+    const temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
+}
 
 function sayHello() {
   console.log("Hello, world!");
