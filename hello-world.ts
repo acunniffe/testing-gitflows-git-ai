c6a41cbf04b38f00512e7357a0891262f31c712e
Aidan Cunniffe <acunniffe@gmail.com>
2025-10-22 08:04:30 -0400

    abc

--- hello-world.ts
+++ hello-world.ts
@@ -26,34 +26,7 @@ function divide(dividend: number, divisor: number): number {
   }
   return result;
 }
-
-// Out of band insertion
 // Line2 
-// Line3
-// Line4
-// Line5
-// Line6
-// Line7
-// Line8
-// Line9
-// Line10
-
-
-// Utilities
-function reverse(str: string): string {
-  return str.split('').reverse().join('');
-}
-// Add two numbers
-function add(a: number, b: number): number {
-  return a + b;
-}
-
-// Subtract two numbers
-function subtract(a: number, b: number): number {
-  return a - b;
-}
-
-// Multiply two numbers
 function multiply(a: number, b: number): number {
   return a * b;
 }
