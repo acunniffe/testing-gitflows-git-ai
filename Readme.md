1757d45894fc3bc36c62f6fb2c2c65095f2285b4
Aidan Cunniffe <acunniffe@gmail.com>
2025-11-19 07:06:50 -0500

    abc

---
+++ Readme.md
@@ -0,0 +1,1 @@
+## A quick demo of Git AI Rewrites
---
+++ set-ops.ts
@@ -0,0 +1,61 @@
+
+// Docs
+export const union = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
+  const _union = new Set<T>(setA);
+  for (const elem of setB) {
+    _union.add(elem);
+  }
+  return _union;
+};
+
+// Docs ABC
+// Docs XYZ
+
+export const intersection = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
+  const _intersection = new Set<T>();
+  for (const elem of setB) {
+    if (setA.has(elem)) {
+      _intersection.add(elem);
+    }
+  }
+  return _intersection;
+};
+
+// Docs YZZ
+export const difference = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
+  const _difference = new Set<T>(setA);
+  for (const elem of setB) {
+    _difference.delete(elem);
+  }
+  return _difference;
+};
+
+export const symmetricDifference = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
+  const _symmetricDifference = new Set<T>(setA);
+  for (const elem of setB) {
+    if (_symmetricDifference.has(elem)) {
+      _symmetricDifference.delete(elem);
+    } else {
+      _symmetricDifference.add(elem);
+    }
+  }
+  return _symmetricDifference;
+};
+
+export const isSuperset = <T>(set: Set<T>, subset: Set<T>): boolean => {
+  for (const elem of subset) {
+    if (!set.has(elem)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export const isSubset = <T>(set: Set<T>, superset: Set<T>): boolean => {
+  for (const elem of set) {
+    if (!superset.has(elem)) {
+      return false;
+    }
+  }
+  return true;
+};
