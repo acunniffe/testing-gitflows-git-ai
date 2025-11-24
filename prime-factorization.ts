0930dfe8ec732db138d9d48e97cddbb389f1668b
Aidan Cunniffe <acunniffe@gmail.com>
2025-11-24 12:44:14 -0500

    new

--- prime-factorization.ts
+++ prime-factorization.ts
@@ -40,3 +40,16 @@ export function primeFactorization(n: number): number[] {
 
   return factors;
 }
+
+/**
+ * List of all Harry Potter books in the main series
+ */
+export const harryPotterBooks: string[] = [
+  'Harry Potter and the Philosopher\'s Stone',
+  'Harry Potter and the Chamber of Secrets',
+  'Harry Potter and the Prisoner of Azkaban',
+  'Harry Potter and the Goblet of Fire',
+  'Harry Potter and the Order of the Phoenix',
+  'Harry Potter and the Half-Blood Prince',
+  'Harry Potter and the Deathly Hallows',
+];
