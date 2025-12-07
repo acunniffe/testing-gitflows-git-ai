f84954efaefc35b5a3c841bb4eeaaf3032e34105
Aidan Cunniffe <acunniffe@gmail.com>
2025-12-07 17:12:25 -0500

    remove (#48)

--- set-ops.ts
+++ set-ops.ts
@@ -1,16 +1,5 @@
 import { APIConnectionError } from "openai"
 
-// ABC
-OTHER STUFF
-
-AI
-AI
-APIConnectionError
-AI
-AIh
-h
-h
-h
 
 // Docs
 export const union = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
