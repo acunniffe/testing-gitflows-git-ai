8503c6e2fdaf659303cf1329cb7fc9e3d4a73075
Aidan Cunniffe <acunniffe@gmail.com>
2025-12-11 08:45:13 -0500

    update

--- set-ops.ts
+++ set-ops.ts
@@ -1,13 +1,6 @@
 import { APIConnectionError } from "openai"
 
 
-// Docs
-export const union = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
-  const _union = new Set<T>(setA);
-  for (const elem of setB) {
-    _union.add(elem);
-  }
-}
 
 export const intersection = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
   const _intersection = new Set<T>();
