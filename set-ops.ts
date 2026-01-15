4c799eaa628be1b9a017b69bc00b7ab819435133
Aidan Cunniffe <acunniffe@gmail.com>
2026-01-14 21:31:17 -0500

    fsdfsd (#64)
    * all stuff
    
    * XYZ (#42)
    
    * new
    
    * new stuff
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * set op
    
    * union intersect
    
    * prove it remembers changes
    
    * abc
    
    * changes
    
    * changes
    
    * algo
    
    * new stuff
    
    * factor
    
    * new stuff
    
    * ai code
    
    * new
    
    * add workflowchanges
    
    * new stuff
    
    * abc (#45)
    
    * abc
    
    * a
    
    * add it
    
    * b
    
    * b
    
    * b
    
    * b
    
    * git-ai otel
    
    * Test branch e2e (#44)
    
    * New branch (#19)
    
    * add two numbers
    
    * subtract
    
    * Branch123 (#20)
    
    * made changes
    
    * abc
    
    * example
    
    * Long running feat (#21)
    
    * pi generator
    
    * count primes
    
    * Branch123 (#22)
    
    * made changes
    
    * abc
    
    * example
    
    * new file
    
    * ai code
    
    * mix
    
    * schedulable
    
    * dry run
    
    * aidan (#35)
    
    * New branch (#19)
    
    * add two numbers
    
    * subtract
    
    * Branch123 (#20)
    
    * made changes
    
    * abc
    
    * example
    
    * Long running feat (#21)
    
    * pi generator
    
    * count primes
    
    * Branch123 (#22)
    
    * made changes
    
    * abc
    
    * example
    
    * Test branch e2e (#32)
    
    * new file
    
    * ai code
    
    * mix
    
    * aidan
    
    * aidan
    
    * aidan
    
    * aidan
    
    * aidan
    
    * daily job
    
    * Branch123 (#23)
    
    * made changes
    
    * abc
    
    * example
    
    * daily
    
    * update
    
    * update
    
    * update
    
    * update
    
    * abc
    
    * new stuff
    
    * new stuff
    
    * actions
    
    * fx version
    
    * to main
    
    * Exampleflow (#47)
    
    * flow
    
    * changes
    
    * change version
    
    * remove (#48)
    
    * A (#49)
    
    * remove
    
    * changes
    
    * daily
    
    * changes
    
    * abc (#50)
    
    * Exampleaaaa (#51)
    
    * abc
    
    * changes
    
    * Exampleaaaa (#52)
    
    * abc
    
    * changes
    
    * changes
    
    * abc (#53)
    
    * Mnn (#54)
    
    * abc
    
    * abc
    
    * abc (#55)
    
    * Mmmmmm (#56)
    
    * abc
    
    * abc
    
    * Mmmmmm (#57)
    
    * abc
    
    * abc
    
    * abc
    
    * Mmmmmm (#58)
    
    * abc
    
    * abc
    
    * abc
    
    * abc
    
    * abc
    
    * Mmmmmm (#59)
    
    * abc
    
    * abc
    
    * abc
    
    * abc
    
    * abc
    
    * modified action
    
    * new workflow
    
    * update (#60)
    
    * Zxx (#61)
    
    * update
    
    * add new action
    
    * do it
    
    * abc
    
    * abc
    
    * pr write
    
    * new home

---
+++ set-ops.ts
@@ -0,0 +1,56 @@
+import { APIConnectionError } from "openai"
+
+
+
+export const intersection = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
+  const _intersection = new Set<T>();
+  for (const elem of setB) {
+    if (setA.has(elem)) {
+      _intersection.add(elem);
+    }
+  }
+  return _intersection;
+
+  
+};
+
+// Docs YZZ
+export const difference = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
+  const _difference = new Set<T>(setA);
+  for (const elem of setB) {
+    _difference.delete(elem);
+  }
+  return _difference;
+};
+
+export const symmetricDifference = <T>(setA: Set<T>, setB: Set<T>): Set<T> => {
+  const _symmetricDifference = new Set<T>(setA);
+  for (const elem of setB) {
+    if (_symmetricDifference.has(elem)) {
+      _symmetricDifference.delete(elem);
+    } else {
+      _symmetricDifference.add(elem);
+    }
+  }
+  return _symmetricDifference;
+};
+
+export const isSuperset = <T>(set: Set<T>, subset: Set<T>): boolean => {
+  for (const elem of subset) {
+    if (!set.has(elem)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export const isSubset = <T>(set: Set<T>, superset: Set<T>): boolean => {
+  for (const elem of set) {
+    if (!superset.has(elem)) {
+      return false;
+    }
+  }
+  return true;
+};
+//ME
+//ME
